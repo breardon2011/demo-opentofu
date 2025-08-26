7b11c8774bc156bf749fe03e75ccdf3021fa257b
Brian Reardon <breardon2011@gmail.com>
2025-08-26 11:02:36 -0700

    change version

--- prod/main.tf
+++ prod/main.tf
@@ -1,19 +1,18 @@
 resource "null_resource" "test" {}
 
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "5.99.0"  # Changed from 5.100.0 to a specific older version
     }
     random = {
       source = "hashicorp/random" 
-      version = "~> 3.5"  # Changed from 3.1
+      version = "3.6.0"  # Changed from 3.7.2 to a specific older version
     }
     null = {
       source = "hashicorp/null"
-      version = "~> 3.2"
+      version = "3.1.0"  # Changed from 3.2.4 to a specific older version
     }
   }
 }
