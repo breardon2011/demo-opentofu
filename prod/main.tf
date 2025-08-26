b0ad31168f1d4c770da54bcffd1a61e515156084
Brian Reardon <breardon2011@gmail.com>
2025-08-26 12:35:28 -0700

    added new versions

--- prod/main.tf
+++ prod/main.tf
@@ -1,18 +1,17 @@
 resource "null_resource" "test" {}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.70.0"  # Different from 5.100.0, 5.99.0, and 5.80.0
+      version = "5.80.0"
     }
     random = {
       source = "hashicorp/random" 
-      version = "3.4.0"  # Different from 3.7.2, 3.6.0, and 3.5.0
+      version = "3.5.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "3.1.1"  # Different from 3.2.4, 3.1.0, and 3.0.0
+      version = "3.0.0"
     }
   }
 }
