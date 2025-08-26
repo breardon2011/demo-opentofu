55e82a73da7ec98f49bd66b00fbfa1f2d9d91a64
Brian Reardon <breardon2011@gmail.com>
2025-08-26 11:10:31 -0700

    third set of versions

--- main.tf
+++ main.tf
@@ -14,15 +14,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.99.0"  # Changed from 5.100.0 to a specific older version
+      version = "5.80.0"
     }
     random = {
       source = "hashicorp/random" 
-      version = "3.6.0"  # Changed from 3.7.2 to a specific older version
+      version = "3.5.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "3.1.0"  # Changed from 3.2.4 to a specific older version
+      version = "3.0.0"
     }
   }
 }
