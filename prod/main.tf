cf05a9c8fa477ca12ec10cec0c3b320a6cb0880a
Brian Reardon <breardon2011@gmail.com>
2025-08-26 12:46:03 -0700

    rotate versions

--- prod/main.tf
+++ prod/main.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.80.0"
+      version = "5.99.0"
     }
     random = {
       source = "hashicorp/random" 
-      version = "3.5.0"
+      version = "3.6.0"
     }
     null = {
       source = "hashicorp/null"
-      version = "3.0.0"
+      version = "3.1.0"
     }
   }
 }
